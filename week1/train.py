6ad5546794a7efaf76ba534710599701aa307cbb
Chowon Moon <mcwmdg429@gmail.com>
2026-08-26 19:05:23 +0900

    feat: Precision, Recall, F1 평가지표 추가

--- week1/train.py
+++ week1/train.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn.metrics import precision_score, recall_score, f1_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
@@ -32,3 +33,10 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 acc = accuracy_score(y_test, y_pred)
 print(f"Accuracy: {acc:.4f}")
+
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+print(f"Precision: {precision:.4f}")
+print(f"Recall: {recall:.4f}")
+print(f"F1 Score: {f1:.4f}")
